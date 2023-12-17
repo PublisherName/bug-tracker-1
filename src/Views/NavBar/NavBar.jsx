91b3d74b2647e3ce3ccbed07b3ba0b7d7ecdf340
publishername <62213479+PublisherName@users.noreply.github.com>
2023-12-17 21:19:19 +0545

    Header -> Navbar

--- src/Views/NavBar/NavBar.jsx
+++ src/Views/NavBar/NavBar.jsx
@@ -6,12 +6,17 @@ import './NavBar.css';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/buglist">Bug List</Link></li>
-      </ul>
-    </nav>
+    <>
+      <header>
+        <h1>Bug Tracker</h1>
+      </header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/buglist">Bug List</Link></li>
+        </ul>
+      </nav>
+    </>
   );
 }
 export default Navbar;
